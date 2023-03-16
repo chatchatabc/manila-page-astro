42314840bf7fa0300c4b6c85cfaf16dda9cd93f3
BoJoNVi <bonjomontes@gmail.com>
2023-03-16 14:07:35 +0800

    build: installed necessary packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://manila.page",
+  integrations: [tailwind(), mdx(), sitemap()],
+});
