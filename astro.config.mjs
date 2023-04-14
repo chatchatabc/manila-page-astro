2f1cb5e2c2c5b5fdaffd989090a42d441f32f0cf
BoJoNVi <bonjomontes@gmail.com>
2023-04-14 09:23:21 +0800

    build: added astro-robots-txt in the project

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,9 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import { astroImageTools } from "astro-imagetools";
+import robotsTxt from "astro-robots-txt";
 
 export default defineConfig({
   site: "https://manila.page",
-  integrations: [tailwind(), mdx(), astroImageTools, sitemap()],
+  integrations: [tailwind(), mdx(), astroImageTools, sitemap(), robotsTxt()],
 });
