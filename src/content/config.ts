1a41fb720d01639ccd5cd0e694f99ccd344bab36
BoJoNVi <bonjomontes@gmail.com>
2023-04-20 11:19:36 +0800

    content(events): Added some contents

--- src/content/config.ts
+++ src/content/config.ts
@@ -8,8 +8,19 @@ const placeSchema = defineCollection({
   }),
 });
 
+const eventSchema = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    summary: z.string(),
+    featuredImage: z.string(),
+    date: z.date(),
+    location: z.string(),
+  }),
+});
+
 export const collections = {
   places: placeSchema,
   trips: placeSchema,
   "foods-and-drinks": placeSchema,
+  events: eventSchema,
 };
