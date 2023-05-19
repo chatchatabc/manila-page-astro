eca1a71196a6648fcedfd00bb56b4bd2b6e1b2a4
BoJoNVi <bonjomontes@gmail.com>
2023-05-19 15:58:17 +0800

    refactor: utilize different color palette

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,19 +8,25 @@ module.exports = {
         play: ["Playfair Display", "serif"],
       },
       colors: {
-        c1: "#009bff", // Primary
-        c2: "#ff6600", // Secondary
-        cl: "#e3f3fe", // Light / Primary 1
-        cd: "#004aad", // Dark / Primary 2
-        ac1: "#6600ff", // Accent 1
-        ac2: "#ff0099", // Accent 2
+        // c1: "#009bff", // Primary
+        // c2: "#ff6600", // Secondary
+        // cl: "#e3f3fe", // Light / Primary 1
+        // cd: "#004aad", // Dark / Primary 2
+        // ac1: "#6600ff", // Accent 1
+        // ac2: "#ff0099", // Accent 2
 
-        // c1: "#FFA303", // Primary
-        // c2: "#A2ABCE", // Secondary
-        // cl: "#FFE0AE", // Light / Primary 1
-        // cd: "#F5631A", // Dark / Primary 2
-        // ac1: "#59ff00", // Accent 1
-        // ac2: "#00ffa6", // Accent 2
+        bg: "#FCF8FD", // Background E0E0FF
+        bg1: "#E0E0FF", // Background 1
+        bg2: "#FFD7F5", // Background 2
+        bg3: "#FFDDB7", // Background 3
+        c1: "#343DFF", // Primary
+        c2: "#9E2A9B", // Secondary
+        c3: "#855400", // Tertiary
+        t1: "#00006E", // Text 1
+        t2: "#380038", // Text 2
+        t3: "#2A1700", // Text 3
+        ac1: "#5d4dbb", // Accent 1
+        ac2: "#ad1788", // Accent 2
       },
     },
   },
