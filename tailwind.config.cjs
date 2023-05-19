73ae5657a8e400f6987d0e2e04523c41eb62ac19
BoJoNVi <bonjomontes@gmail.com>
2023-05-19 10:23:13 +0800

    refactor: utilized a different color palette

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,9 +8,19 @@ module.exports = {
         play: ["Playfair Display", "serif"],
       },
       colors: {
-        primary: "#BFD7EA",
-        text: "#000",
-        background: "#BFD7EA",
+        c1: "#009bff", // Primary
+        c2: "#ff6600", // Secondary
+        cl: "#e3f3fe", // Light / Primary 1
+        cd: "#004aad", // Dark / Primary 2
+        ac1: "#6600ff", // Accent 1
+        ac2: "#ff0099", // Accent 2
+
+        // c1: "#FFA303", // Primary
+        // c2: "#A2ABCE", // Secondary
+        // cl: "#FFE0AE", // Light / Primary 1
+        // cd: "#F5631A", // Dark / Primary 2
+        // ac1: "#59ff00", // Accent 1
+        // ac2: "#00ffa6", // Accent 2
       },
     },
   },
